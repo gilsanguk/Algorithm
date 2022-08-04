ab30e3c5412ce797a0ab242158cd67dc89cad034
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-08-04 22:21:14 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+	for (int i = T; i >=1; i--) {
+		cout << i << "\n";
+	}
+}
