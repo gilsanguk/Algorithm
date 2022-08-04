c234909db99271d8ad5ddb8de6c8a2991b44215e
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-08-04 22:38:36 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+	for(int i=1;i<=T;i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << "Case #" << i << ": "<<a<<" + "<<b<<" = "<<a + b << "\n";
+	}
+}
