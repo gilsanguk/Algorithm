69295b0b72f68b075acd7884868775fbd812c359
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-08-04 22:32:19 +0900

    [Bronze III] Title: 최소, 최대, Time: 524 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1000000];
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	cout << arr[0] << " " << arr[N - 1];
+}
