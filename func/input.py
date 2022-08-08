bbd2b0ed1c9018943d1624c6f2d480749e28ab74
이수민 <sumin940305@gmail.com>
2022-08-09 08:59:22 +0900

    algorithm

--- func/input.py
+++ func/input.py
@@ -7,9 +7,13 @@ li = list(map(int, stdin.readline().split()))
 n, m = map(int, stdin.readline().split())
 # 2차원 배열 받기
 li = []
-for i in range(n):
+for _ in range(n):
     li.append(list(map(int, stdin.readline().split())))
 # 한줄 씩 인풋 받을 때
 li = [0] * n
 for i in range(n):
     li[i] = int(stdin.readline())
+# 테스트 케이스 출력
+for T in range(int(stdin.readline())):
+
+    print(f'#{T+1}')
