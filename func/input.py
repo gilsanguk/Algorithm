b50ff7fd2f08a0c3826635fa120cc40105e9006f
길상욱 <sanguk9805@naver.com>
2022-08-12 09:26:29 +0900

    algorithm

--- func/input.py
+++ func/input.py
@@ -1,21 +1,24 @@
 import sys
+input = sys.stdin.readline
 # 텍스트 파일 인풋
 sys.stdin = open('input.txt')
 # 한가지 수 받기
-n = int(sys.stdin.readline())
+n = int(input())
 # 리스트로 받기
-li = list(map(int, sys.stdin.readline().split()))
+li = list(map(int, input().split()))
 # 여러가지 수 받기
-n, m = map(int, sys.stdin.readline().split())
+n, m = map(int, input().split())
 # 2차원 배열 받기
 li = []
 for _ in range(n):
-    li.append(list(map(int, sys.stdin.readline().split())))
+    li.append(list(map(int, input().split())))
+# list comprehension
+li = [(list(map(int, input().split()))) for _ in range(n)]
 # 한줄 씩 인풋 받을 때
 li = [0] * n
 for i in range(n):
-    li[i] = int(sys.stdin.readline())
+    li[i] = int(input())
 # 테스트 케이스 출력
-for T in range(int(sys.stdin.readline())):
+for T in range(int(input())):
 
     print(f'#{T+1}')
