10db7eaab4a1ef63a7413a17bcb7cbb1f6174da2
길상욱 <gilsang-uk@gilsang-ug-ui-MacBookPro.local>
2022-08-19 02:18:19 +0900

    algorithm

---
+++ func/BFS.py
@@ -0,0 +1,21 @@
+from collections import deque
+def BFS(li,y,x):
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+    visited = [[1]*len(li[0]) for _ in range(len(li))]
+    q = deque()
+    q.append((y,x))
+    visited[y][x] = 0
+    cnt = 0
+
+    while q:
+        cnt += 1
+        for i in range(len(q)):
+            y,x = q.popleft()
+            for i in range(4):
+                tmp_y, tmp_x = y+dy[i], x+dx[i]
+                if 0 <= tmp_y < len(li) and 0 <= tmp_x < len(li[0]):
+                    if li[tmp_y][tmp_x] == 'L' and visited[tmp_y][tmp_x]: 
+                        q.append((tmp_y, tmp_x))
+                        visited[tmp_y][tmp_x] = 0
+    return cnt-1
