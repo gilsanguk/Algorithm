c2ae1e8c363007301eef8b956017fc8acd886446
길상욱 <sanguk9805@naver.com>
2022-08-19 09:15:24 +0900

    algorithm

--- func/DFS.py
+++ func/DFS.py
@@ -10,3 +10,19 @@ def DFS(graph,route):
                 continue
             stack += graph[n] - visited
     return visited
+
+V, E = map(int, input().split())    # 정점수, 간선수
+G = [[0] * (V + 1) for _ in range(V + 1)]   # 인접 행렬
+visited = [0] * (V + 1)
+# 간선수만큼 입력 처리
+for _ in range(E):
+    u, v = map(int, input().split())
+    G[u][v] = G[v][u] = 1
+
+def dfs(v):
+    visited[v] = 1; print(v, end=' ')
+    for w in range(1, V + 1):
+        if G[v][w] == 1 and not visited[w]:
+            dfs(w)
+
+dfs(1)
