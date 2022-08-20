3bf2214498865df941464a4bdc74283267af23ca
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-08-20 23:14:06 +0900

    [Gold V] Title: 공유기 설치, Time: 620 ms, Memory: 40684 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,33 +1,30 @@
-import sys;input = sys.stdin.readline
+import math,sys
+input = sys.stdin.readline
 
-n, c = map(int, input().split())
+n,c = map(int,input().split())
+h = [int(input()) for i in range(n)]
+h.sort()
+start,end = 1, h[n-1] - h[0]
+# 집 사이의 최소 거리, 최대 거리
+result = 0
 
+if c == 2:
+    print(h[n-1] - h[0])
+    # 집이 2개라면 무조건 처음, 마지막 집 사이의 거리
+else:
+    while(start < end):
+        mid = (start + end)//2
 
-def solve():
-    s, e = 1, li[-1] - li[0]
-    res = 0
-    while s <= e:
-        mid = (s + e) // 2
-        curr = li[0]
-        cnt = 1
-        tmp = 1000000000
-        for i in range(1, n):
-            if li[i] >= curr + mid:
-                tmp = min(tmp, li[i]-curr)
-                cnt += 1
-                curr = li[i]
-
-        if cnt < c:
-            e = mid-1
-        else:
-            s = mid+1
-            res = max(res, tmp)
-
-    return res
-
-
-li = [0] * n
-for i in range(n):
-    li[i] = int(input())
-li.sort()
-print(solve())
+        count = 1
+        ts = h[0]
+        # 마지막으로 설치된 공유기의 위치
+        for i in range(n):
+            if h[i] - ts >= mid:
+                count+=1
+                ts = h[i]
+        if count >= c:
+            result = mid
+            start = mid + 1
+        elif count < c:
+            end = mid
+    print(result)
