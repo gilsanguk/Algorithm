339a7afd474f8cfe59fc0345c0c6e21d982b631b
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-08-26 02:53:01 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 244 ms, Memory: 48524 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -8,10 +8,9 @@ for i in range(1, n + 1):
     poketmon = input().rstrip()
     poket_dct[poketmon] = i
     poket_li[i] = poketmon
-digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 for _ in range(m):
     tmp = input().rstrip()
-    if tmp[0] in digit:
+    if tmp.isdecimal():
         print(poket_li[int(tmp)])
     else:
         print(poket_dct[tmp])
