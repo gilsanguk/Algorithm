5497ebe50415d029b5a87adda1f78c6a1d8d5d31
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-08-26 02:50:00 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 268 ms, Memory: 48524 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,13 +1,17 @@
-n, m = map(int, input().split())
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().rstrip().split())
 poket_dct = {}
-poket_li = [0] * (n+1)
-for i in range(1,n+1):
-    poketmon = input()
+poket_li = [0] * (n + 1)
+for i in range(1, n + 1):
+    poketmon = input().rstrip()
     poket_dct[poketmon] = i
     poket_li[i] = poketmon
+digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 for _ in range(m):
-    tmp = input()
-    if tmp.isdecimal():
+    tmp = input().rstrip()
+    if tmp[0] in digit:
         print(poket_li[int(tmp)])
     else:
         print(poket_dct[tmp])
