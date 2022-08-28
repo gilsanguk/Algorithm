df1081dda16b01c602a1750a124f03b48f63b79b
길상욱 <gilsang-uk@gilsang-ug-ui-MacBookPro.local>
2022-08-28 11:14:39 +0900

    algorigthm

--- func/perm, comb.py
+++ func/perm, comb.py
@@ -1,27 +1,40 @@
-def perm(k, r):
+def perm(k):
     if k == r:
         print(*order)
     else:
         for i in range(n):
-            if visited[i]: continue
-            order[k] = li[i]
-            visited[i] = 1
-            perm(k + 1, r)
-            visited[i] = 0
+            if not visited[i]:
+                order[k] = li[i]
+                visited[i] = 1
+                perm(k + 1)
+                visited[i] = 0
+
+def comb(k, l):
+    if k == r:
+        print(*order)
+    else:
+        for i in range(l, n):
+            if not visited[i]:
+                order[k] = li[i]
+                visited[i] = 1
+                comb(k + 1, i+1)
+                visited[i] = 0
+
+# def comb(k, res):
+#     if len(res) == r:
+#         print(*res)
+#         return
+
+#     for i in range(k, n):
+#         comb(i + 1, res + [li[i]])
 
 
-def comb(k, res):
-    if len(res) == r:
-        print(*res)
-        return
-
-    for i in range(k, n):
-        comb(i + 1, res + [li[i]])
 
 
 n, r = map(int, input().split())
 li = [1, 2, 3, 4]
 visited = [0] * n
 order = [0] * r
-# perm(0, r)
-comb(0, [])
+perm(0)
+print()
+comb(0, 0)
