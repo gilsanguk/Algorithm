d0991b7648197212ff1ff68efb83e823f5c622b5
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-08-28 11:52:41 +0900

    [Silver II] Title: N과 M (10), Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,25 @@
+import sys;input = sys.stdin.readline
+def perm(k, l):
+    if k == m:
+        tmp = res[:]
+        result.append(tmp)
+        return
+    for i in range(l, n):
+        if not visited[i]:
+            visited[i] = 1
+            res[k] = li[i]
+            perm(k+1, i+1)
+            visited[i] = 0
+
+
+n, m = map(int,input().split())
+li = list(map(int,input().split()))
+visited = [0] * n
+res = [0] * m
+result = []
+li.sort()
+perm(0, 0)
+result = list(set([tuple(item) for item in result]))
+result.sort()
+for item in result:
+    print(*item)
