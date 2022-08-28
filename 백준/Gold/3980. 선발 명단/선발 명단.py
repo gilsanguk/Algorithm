88ba6bf67d378508214c8617f12ce8a906dd6bf1
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-08-28 12:39:00 +0900

    [Gold V] Title: 선발 명단, Time: 188 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/3980. 선발 명단/선발 명단.py
@@ -0,0 +1,22 @@
+import sys;input = sys.stdin.readline
+
+def perm(row, overall):
+    global res
+    if 100 * (n-row) + overall < res:
+        return
+    if row == n:
+        res = max(res, overall)
+        return
+    for i in range(n):
+        if li[row][i] != 0 and i not in visited:
+            visited[row] = i
+            perm(row+1, overall+li[row][i])
+            visited[row] = -1
+
+for _ in range(int(input())):
+    n = 11
+    li = [list(map(int,input().split())) for _ in range(n)]
+    visited = [-1] * n
+    res = 0
+    perm(0, 0)
+    print(res)
