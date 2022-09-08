900c6b4b0aaddf154d93955c920ba3d6f1039a2e
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-09-08 15:47:09 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 5184 ms, Memory: 478016 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -0,0 +1,31 @@
+import sys;input = sys.stdin.readline
+from collections import deque
+
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
+
+
+def bfs():
+    cnt = 0
+    q = deque()
+    q.append((0, 0, 0))
+    while q:
+        cnt += 1
+        for _ in range(len(q)):
+            y, x, check = q.popleft()
+            if y == n - 1 and x == m - 1: return cnt
+            for i in range(4):
+                ny, nx = y + dy[i], x + dx[i]
+                if 0 <= ny < n and 0 <= nx < m and not visited[ny][nx][check]:
+                    visited[ny][nx][check] = 1
+                    if li[ny][nx] == 0:
+                        q.append((ny, nx, check))
+                    elif check < k:
+                        q.append((ny, nx, check + 1))
+    return -1
+
+
+n, m, k = map(int, input().split())
+li = [list(map(int, input().rstrip())) for _ in range(n)]
+visited = [[[0] * (k + 1) for _ in range(m)] for _ in range(n)]
+print(bfs())
