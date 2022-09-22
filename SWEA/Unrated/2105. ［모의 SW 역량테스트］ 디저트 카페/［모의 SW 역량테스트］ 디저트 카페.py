fb7930b4adea3a3b3c0c3804576dd9c35f16397d
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-09-23 06:13:06 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 562 ms, Memory: 66,748 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
@@ -1,5 +1,6 @@
 dy = [1, 1, -1, -1]
 dx = [1, -1, -1, 1]
+
 def dfs(sy, sx, y, x, d, cnt):
     global res, visited
     if not (0 <= y < N and 0 <= x < N): return
@@ -13,6 +14,7 @@ def dfs(sy, sx, y, x, d, cnt):
         dfs(sy, sx, y + dy[d + 1], x + dx[d + 1], d + 1, cnt + 1)
     visited[arr[y][x]] = False
 
+
 for T in range(int(input())):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
@@ -20,5 +22,6 @@ for T in range(int(input())):
     res = -1
     for y in range(N - 2):
         for x in range(1, N - 1):
+            if res >= (N - y - 1) * 2 + 1: break
             dfs(y, x, y+1, x+1, 0, 1)
     print(f'#{T + 1}', res)
