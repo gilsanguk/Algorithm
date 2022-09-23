76fe491de248cfd2b159f6dbaf2be049da671a9f
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-09-23 10:32:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 152 ms, Memory: 115112 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,12 +1,10 @@
-import sys;input = sys.stdin.readline
 n, k = map(int, input().split())
 li = [tuple(map(int, input().split())) for _ in range(n)]
-dp = [[0] * (k + 1) for _ in range(n + 1)]
-for i in range(1, n + 1):
-    w, v = li[i - 1]
-    for j in range(1, k + 1):
-        if w > j:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
-print(dp[n][k])
+li.sort()
+dp = [0] * (k+1)
+for w, v in li:
+    for i in range(k, -1, -1):
+        if i < w:
+            break
+        dp[i] = max(dp[i-w] + v, dp[i])
+print(dp[k])
