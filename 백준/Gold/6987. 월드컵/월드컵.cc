3ca1c5b583a79bad6bfbda90c250b83ac4f7d7d6
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-10-02 22:53:23 +0900

    [Gold V] Title: 월드컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.cc
+++ 백준/Gold/6987. 월드컵/월드컵.cc
@@ -3,23 +3,22 @@
 #include <algorithm>
 using namespace std;
 
-int scores[6][3], res;
-vector<pair<int, int>> games;
+int scores[6][3];
 vector<pair<int, int>> delta = {{1, 1}, {0, 2}, {2, 0}};
 
-int solve(int curr){
-    if (curr == 15){
+int solve(int home, int away){
+    if (home == 5){
         for (int i = 0; i < 6; i++)
             for (int j = 0; j < 3; j++)
                 if (scores[i][j]) return false;
         return true;
     }
-    auto [home, away] = games[curr];
     for (auto [a, b] : delta){
         if (scores[home][a] && scores[away][b]){
             scores[home][a]--;
             scores[away][b]--;
-            if (solve(curr + 1)) return true;
+            if (away == 5) {if (solve(home + 1, home + 2)) return true;}
+            else {if (solve(home, away + 1)) return true;}
             scores[home][a]++;
             scores[away][b]++;
         }
@@ -31,19 +30,12 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
-    for (int i = 0; i < 6; i++){
-        for (int j = i + 1; j < 6; j++){
-            games.push_back({i,j});
-        }
-    }
-
     for (int i = 0; i < 4; i++){
         for (int j = 0; j < 6; j++) {
             for (int k = 0; k < 3; k++) {
                 cin >> scores[j][k];
             }
         }
-        cout << solve(0) << ' ';
-        res = 0;
+        cout << solve(0, 1) << ' ';
     }
 }
