aa94cfc3d2fbaa641716f48cfee9aa1265265e45
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-10-04 21:51:40 +0900

    [level 3] Title: N으로 표현, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -4,22 +4,21 @@ def getN(i, N):
         ret = ret * 10 + N
         i -= 1
     return ret
-
 def solution(N, number):
-    dp = [[] for _ in range(9)]
+    dp = [set() for _ in range(9)]
     dp[1] = [N]
     if N == number: return 1
     for i in range(2, 9):
         tmp = getN(i, N)
         if tmp == number: return i
-        dp[i].append(tmp)
+        dp[i].add(tmp)
         for j in range(1, i):
             for num in dp[j]:
                 if num == 0: continue
                 for num2 in dp[i-j]:
                     if num2 == 0: continue
                     a, b, c, d = num + num2, num - num2, num * num2, num // num2
-                    tmp = [a, b, c, d]
-                    if number in tmp: return i
-                    dp[i] += tmp
+                    for tmp in [a, b, c, d]:
+                        if number == tmp: return i
+                        dp[i].add(tmp)
     return -1
