d9dd3abfb4abddd6c3294afcda90e9d7c6118f91
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-10-10 21:53:05 +0900

    [Gold II] Title: 색종이 붙이기, Time: 1192 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.py
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.py
@@ -2,7 +2,7 @@ import sys;input=sys.stdin.readline
 def possible(y, x, size):
     for r in range(y, y+size):
         for c in range(x, x+size):
-            if r >= 10 or c >= 10 or board[r][c] == 0:
+            if dp[r][c] == 0:
                 return False
     return True
 
@@ -12,23 +12,31 @@ def backtrack(y, x, cnt):
     if y > 9:
         res = min(res, cnt)
         return
-    if board[y][x] == 0: backtrack(y, x+1, cnt) if x < 9 else backtrack(y+1, 0, cnt)
-    else:
-        for size in reversed(range(1, 6)):
+
+    if dp[y][x] > 0:
+        for size in reversed(range(1, dp[y][x] + 1)):
             if visited[size] == 5: continue
-            if y+size > 10 or x+size > 10: continue
             if not possible(y, x, size): continue
             for r in range(y, y+size):
                 for c in range(x, x+size):
-                    board[r][c] = 0
+                    dp[r][c] = 0
             visited[size] += 1
-            backtrack(y, x+size, cnt+1) if x+size <= 9 else backtrack(y+1, 0, cnt+1)
+            backtrack(y, x+size, cnt+1) if x+size < 10 else backtrack(y+1, 0, cnt+1)
             visited[size] -= 1
             for r in range(y, y+size):
                 for c in range(x, x+size):
-                    board[r][c] = 1
+                    dp[r][c] = board[r][c]
+    else: backtrack(y, x+1, cnt) if x < 9 else backtrack(y+1, 0, cnt)
+
+def ignite():
+    for y in reversed(range(9)):
+        for x in reversed(range(9)):
+            board[y][x] = (min(board[y+1][x], board[y][x+1], board[y+1][x+1]) + 1) * board[y][x]
+            if board[y][x] > 5: board[y][x] = 5
 
 board = [list(map(int, input().split())) for _ in range(10)]
+ignite()
+dp = [row[:] for row in board]
 visited = [0] * 6
 res = 26
 backtrack(0, 0, 0)
