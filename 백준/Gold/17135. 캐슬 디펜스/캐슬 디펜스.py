7f52cc1b977ccb6ea3fbb5dd922fc8d7696357b2
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-10-13 13:16:36 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 352 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
@@ -0,0 +1,41 @@
+import sys;input=sys.stdin.readline
+from itertools import combinations as comb
+def attack(archer, board):
+    for d in range(1, D + 1):
+        y, x = N, archer - d
+        for i in range(d * 2 - 1):
+            x = x + 1
+            y = y - 1 if i < d else y + 1
+            if 0 > x or x >= M or 0 > y or y >= N: continue
+            if board[y][x]:
+                ret = 1 if board[y][x] == 1 else 0
+                board[y][x] = -1
+                return ret
+    return 0
+
+def move(board):
+    for x in range(M):
+        for y in reversed(range(1, N)):
+            board[y][x] = board[y-1][x]
+            if board[y][x] == -1: board[y][x] = 0
+        board[0][x] = 0
+
+def solve(row, archers, board):
+    global ans, cnt
+    if row == N:
+        ans = max(ans, cnt)
+        return
+    for archer in archers: cnt += attack(archer, board)
+    move(board)
+    solve(row+1, archers, board)
+
+
+N, M, D = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(N)]
+ans = 0
+cnt = 0
+for archers in comb(range(M), 3):
+    copy_board = [board[i][:] for i in range(N)]
+    cnt = 0
+    solve(0, archers, copy_board)
+print(ans)
