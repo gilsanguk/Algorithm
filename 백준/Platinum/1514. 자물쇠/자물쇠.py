55c7cf052a89c67239629bdc44cd0c559bbcc78c
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-10-15 13:43:57 +0900

    [Platinum III] Title: 자물쇠, Time: 1796 ms, Memory: 41840 KB -BaekjoonHub

--- 백준/Platinum/1514. 자물쇠/자물쇠.py
+++ 백준/Platinum/1514. 자물쇠/자물쇠.py
@@ -1,29 +1,27 @@
 import sys;input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+def mod(a, b, d): return (a + b) % 10 if d == 1 else (a - b + 10) % 10
 
-def mod(a,b,c):
-    return (a+b)%10 if c > 0 else (a-b+10)%10
+def solve(idx, x, y, z, d):
+    if idx == N: return 0
+    if dp[idx][x][y][z][d] != -1: return dp[idx][x][y][z][d]
 
-def go(idx,x,y,z,d):
-    if idx == n:
-        return 0
-    if dp[idx][x][y][z][d] != -1:
+    dp[idx][x][y][z][d] = 1987654321
+    if (int(From[idx]) + x) % 10 == int(To[idx]):
+        dp[idx][x][y][z][d] = min(solve(idx + 1, y, z, 0, d), solve(idx + 1, y, z, 0, d ^ 1))
         return dp[idx][x][y][z][d]
-    dp[idx][x][y][z][d] = 2100000000
-    if (int(s[idx]) + x)%10 == int(e[idx]):
-        dp[idx][x][y][z][d] = min(go(idx + 1, y, z, 0, int(not d)), go(idx + 1, y, z, 0, d))
-        return dp[idx][x][y][z][d]
-    
-    for k in range(1,4):
+
+    for turn in range(1, 4):
         dp[idx][x][y][z][d] = min(
-            dp[idx][x][y][z][d], go(idx, mod(x, k, d), y, z, d) + 1)
+            dp[idx][x][y][z][d], solve(idx, mod(x, turn, d), y, z, d) + 1)
         dp[idx][x][y][z][d] = min(
-            dp[idx][x][y][z][d], go(idx, mod(x, k, d), mod(y,k,d),z,d) + 1)
+            dp[idx][x][y][z][d], solve(idx, mod(x, turn, d), mod(y, turn, d), z, d) + 1)
         dp[idx][x][y][z][d] = min(
-            dp[idx][x][y][z][d], go(idx, mod(x, k, d), mod(y, k, d), mod(z, k, d), d) + 1)
+            dp[idx][x][y][z][d], solve(idx, mod(x, turn, d), mod(y, turn, d), mod(z, turn, d), d) + 1)
     return dp[idx][x][y][z][d]
 
-n = int(input())
-s = input()
-e = input()
-dp = [[[[[-1]* 2 for _ in range(10)]for _ in range(10)]for _ in range(10)]for _ in range(101)]
-print(min(go(0,0,0,0,0), go(0,0,0,0,1)))
+N = int(input())
+From = input()
+To = input()
+dp = [[[[[-1] * 2 for _ in range(10)] for _ in range(10)] for _ in range(10)] for _ in range(101)]
+print(min(solve(0, 0, 0, 0, 0), solve(0, 0, 0, 0, 1)))
