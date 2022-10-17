b60c4da1d00666420d4dea52b0930af6bdb8ba6e
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-10-17 13:24:22 +0900

    [Silver III] Title: 퇴사, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,20 +1,8 @@
 import sys;input = sys.stdin.readline
-
 n = int(input())
-arr = [list(map(int,input().split())) for _ in range(n)]
-ans = 0
-for i in range(1 << n):
-    visited = [False] * (n+1)
-    res, tmp = 0, False
-    for j in range(n):
-        if i & (1 << j):
-            if arr[j][0] + j > n: break
-            for k in range(j,j+arr[j][0]):
-                if visited[k]:
-                    tmp = True
-                    break
-                visited[k] = True
-            res += arr[j][1]
-        if tmp: break
-        ans = max(res, ans)
-print(ans)
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [0] * (n + 1)
+for i in range(n - 1 , -1, -1):
+    if arr[i][0] + i > n: dp[i] = dp[i+1]
+    else: dp[i] = max(dp[i+1], dp[i + arr[i][0]]+ arr[i][1])
+print(dp[0])
