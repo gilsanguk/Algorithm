2b1c8595905c6a73a52b5b751c38109fd8a89bbb
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-10-21 16:47:27 +0900

    [Gold IV] Title: 팰린드롬?, Time: 272 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+int N, M, S, E;
+int arr[2001];
+int dp[2001][2001];
+
+int palindrom(int s, int e) {
+	int& ret = dp[s][e];
+	if (arr[s] != arr[e]) return ret = 0;
+	if (ret != -1) return ret;
+	ret = 1;
+	if (s + 1 < e - 1) {
+		ret &= palindrom(s + 1, e - 1);
+	}
+	return ret;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	memset(dp, -1, sizeof(dp));
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+	}
+	cin >> M;
+	while (M--) {
+		cin >> S >> E;
+		cout << palindrom(S, E) << '\n';
+	}
+}
