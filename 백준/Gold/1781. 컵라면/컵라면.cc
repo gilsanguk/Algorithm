0cc313070811b35b423dd353c642de9f08536286
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-10-23 00:42:55 +0900

    [Gold II] Title: 컵라면, Time: 80 ms, Memory: 6244 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int N, W, V;
+priority_queue<pair<int,int>> pq;
+priority_queue<int> possible;
+
+int solve(){
+    int ret = 0;
+    for (int i = N; i > 0; i--) {
+        while (!pq.empty() && pq.top().first >= i) {
+            possible.push(pq.top().second);
+            pq.pop();
+        }
+        if (!possible.empty()) {
+            ret += possible.top();
+            possible.pop();
+        }
+    }
+    return ret;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> W >> V;
+        pq.push({W, V});
+    }
+    cout << solve() << '\n';
+}
