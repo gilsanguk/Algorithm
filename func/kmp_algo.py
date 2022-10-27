d80ff08a62599880730b2e29d7c74f1b9551831c
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-10-28 08:54:31 +0900

    Update kmp_algo.py

--- func/kmp_algo.py
+++ func/kmp_algo.py
@@ -1,32 +1,20 @@
-def KMP(t, p):
+def kmp(t, p):
     n, m = len(t), len(p)
     lps = [0] * m
-    LPS(p,lps)
-    i, j = 0, 0
-    while i < n:
-        if t[i] == p[j]:
+    i = 0
+    for j in range(1, m):
+        while i and p[i] != p[j]:
+            i = lps[i-1]
+        if p[i] == p[j]:
             i += 1
-            j += 1
-        elif t[i] != p[j]:
-            if j != 0:
-                j = lps[j-1]
-            else:
-                i += 1
-        if j == m:
-            print('!') # 출력
-            j = lps[j-1] # 다시 탐색
-
-def LPS(p,lps):
-    l = 0
-    i = 1
-    while i < len(p):
-        if p[l] == p[i]:
-            l += 1
-            lps[i] = l
+            lps[j] = i
+    i, cnt = 0, 0
+    for j in range(n):
+        while i and p[i] != t[j]:
+            i = lps[i-1]
+        if p[i] == t[j]:
             i += 1
-        else:
-            if l != 0:
-                l = lps[l-1]
-            else:
-                lps[i] = 0
-                i += 1
+            if i == m:
+                cnt += 1
+                i = lps[i-1]
+    return cnt
