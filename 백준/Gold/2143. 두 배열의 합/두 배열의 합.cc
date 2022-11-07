8f616672f764aec75b7def5689091099c4dcf2ca
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-11-07 15:33:11 +0900

    [Gold III] Title: 두 배열의 합, Time: 312 ms, Memory: 41104 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -1,27 +1,27 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 using ll = long long;
 
 int T, N, M;
 int A[1001], B[1001];
-map<int, int> PmapA;
-ll solve(){
-    ll ret = 0;
+unordered_map<int, int> PsumA;
+void solve(){
+    ll ans = 0;
     for(int i = 0; i < N; i++){
         for(int j = i; j < N; j++){
             int sum = A[j+1] - A[i];
-            PmapA[sum]++;
+            PsumA[sum]++;
         }
     }
     for(int i = 0; i < M; i++){
         for(int j = i; j < M; j++){
             int sum = B[j+1] - B[i];
-            ret += PmapA[T-sum];
+            ans += PsumA[T-sum];
         }
     }
-    return ret;
+    printf("%lld", ans);
 }
 
 int main() {
@@ -32,16 +32,12 @@ int main() {
     scanf("%d", &N);
     for (int i = 1; i <= N; i++) {
         scanf("%d", &A[i]);
+        A[i] = A[i-1] + A[i];
     }
     scanf("%d", &M);
     for (int i = 1; i <= M; i++) {
         scanf("%d", &B[i]);
+        B[i] = B[i - 1] + B[i];
     }
-    for (int i = 1; i <= N; i++) {
-        A[i] = A[i-1] + A[i];
-    }
-    for (int i = 1; i <= M; i++) {
-        B[i] = B[i-1] + B[i];
-    }
-    printf("%lld", solve());
+    solve();
 }
