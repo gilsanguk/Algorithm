65e92c8a40e6954e1e20ec642887d0006c27cecd
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-11-07 15:32:12 +0900

    [Gold III] Title: 두 배열의 합, Time: 432 ms, Memory: 43084 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -6,22 +6,22 @@ using ll = long long;
 
 int T, N, M;
 int A[1001], B[1001];
-map<int, int> PsumA;
-void solve(){
-    ll ans = 0;
+map<int, int> PmapA;
+ll solve(){
+    ll ret = 0;
     for(int i = 0; i < N; i++){
         for(int j = i; j < N; j++){
             int sum = A[j+1] - A[i];
-            PsumA[sum]++;
+            PmapA[sum]++;
         }
     }
     for(int i = 0; i < M; i++){
         for(int j = i; j < M; j++){
             int sum = B[j+1] - B[i];
-            ans += PsumA[T-sum];
+            ret += PmapA[T-sum];
         }
     }
-    printf("%lld", ans);
+    return ret;
 }
 
 int main() {
@@ -32,12 +32,16 @@ int main() {
     scanf("%d", &N);
     for (int i = 1; i <= N; i++) {
         scanf("%d", &A[i]);
-        A[i] = A[i-1] + A[i];
     }
     scanf("%d", &M);
     for (int i = 1; i <= M; i++) {
         scanf("%d", &B[i]);
-        B[i] = B[i - 1] + B[i];
     }
-    solve();
+    for (int i = 1; i <= N; i++) {
+        A[i] = A[i-1] + A[i];
+    }
+    for (int i = 1; i <= M; i++) {
+        B[i] = B[i-1] + B[i];
+    }
+    printf("%lld", solve());
 }
