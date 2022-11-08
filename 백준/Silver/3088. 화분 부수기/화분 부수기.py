ac69cf49d297a782e3cbe84cd60c35bcaaf5ed5e
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2022-11-08 09:04:19 +0900

    [Silver II] Title: 화분 부수기, Time: 544 ms, Memory: 38652 KB -BaekjoonHub

--- 백준/Silver/3088. 화분 부수기/화분 부수기.py
+++ 백준/Silver/3088. 화분 부수기/화분 부수기.py
@@ -1,10 +1,14 @@
 import sys;input = sys.stdin.readline
-n = int(input())
-brk_li = [0] * 1000001
-cnt = 0
-for i in range(n):
-    a, b, c = map(int, input().split())
-    if brk_li[a] == 0 and brk_li[b] == 0 and brk_li[c] == 0:
-        cnt += 1
-    brk_li[a], brk_li[b], brk_li[c] = brk_li[a]+1, brk_li[b]+1, brk_li[c]+1
-print(cnt)
+N = int(input())
+check_plate = [0] * 1000001
+count = 0
+
+for p in range(N):
+    plate = list(map(int,input().split()))
+    if check_plate[plate[0]] != 1 and check_plate[plate[1]] != 1 and check_plate[plate[2]] != 1:
+        count += 1
+    check_plate[plate[0]] = 1
+    check_plate[plate[1]] = 1
+    check_plate[plate[2]] = 1
+
+print(count)
