420df88d081ec114ca34e01f77cfd709bf7158c0
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2023-03-09 01:08:13 +0900

    [Gold V] Title: 치킨 배달, Time: 536 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,19 +1,36 @@
-import sys;input = sys.stdin.readline
 from itertools import combinations
 
-n, m = map(int,input().split())
-li = [list(map(int,input().split())) for _ in range(n)]
-hom, chi, ans = [], [], 987654321
-for y in range(n):
-    for x in range(n):
-        if li[y][x] == 1: hom.append((y,x))
-        if li[y][x] == 2: chi.append((y,x))
-for c in combinations(chi, m):
-    res = 0
-    for hy, hx in hom:
-        tmp = 987654321
-        for cy, cx in c:
-            tmp = min(abs(hy-cy) + abs(hx - cx), tmp)
-        res += tmp
-    ans = min(ans, res)
-print(ans)
+# 치킨과 집 사이의 거리 구하기
+def distance(chicken_house):
+    home_chicken = 0
+    for i in range(N):
+        for j in range(N):
+            if maps[i][j] == 1:
+                a = 100
+                for k in range(M):
+                    b = abs(i-chicken_house[k][0]) + abs(j-chicken_house[k][1])
+                    a = min(a, b)
+                home_chicken += a
+    return home_chicken
+
+# 입력값 받기
+N, M = map(int, input().split())
+maps = [list(map(int, input().split())) for i in range(N)]
+chicken = []
+for i in range(N):
+    for j in range(N):
+        if maps[i][j] == 2:
+            chicken.append((i, j))
+
+# 치킨 조합으로 만들기
+chicken_set = list(combinations(chicken,M))
+min_result = 987654321
+
+
+# 이제 뭐가 나은지 확인하기
+for i in chicken_set:
+    result = distance(i)
+    if min_result > result:
+        min_result = result
+
+print(min_result)
