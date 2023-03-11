2ba49e06add5688e04349cf895fdc4730cc915ca
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2023-03-11 23:41:21 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 4264 ms, Memory: 191064 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,28 +1,30 @@
-import sys;input = sys.stdin.readline
 from collections import deque
 
-dy = [-1, 0, 1, 0]
-dx = [0, 1, 0, -1]
 def bfs():
-    cnt = 0
-    q = deque()
-    q.append((0, 0, 0))
-    while q:
-        cnt += 1
-        for _ in range(len(q)):
-            y, x, check = q.popleft()
-            if y == n - 1 and x == m - 1: return cnt
-            for i in range(4):
-                ny, nx = y + dy[i], x + dx[i]
-                if 0 <= ny < n and 0 <= nx < m and not visited[ny][nx][check]:
-                    visited[ny][nx][check] = 1
-                    if li[ny][nx] == 0:
-                        q.append((ny, nx, check))
-                    elif check == 0:
-                        q.append((ny, nx, 1))
+    Q = deque()
+    Q.append((0,0,False))
+    while Q:
+        y1, x1, wall = Q.popleft()
+        # 도착했으면 return 하기
+        if y1 == N-1 and x1 == M-1:
+            return abs(visited[y1][x1][wall])
+        for dy, dx in ((0,1), (1,0), (0,-1), (-1,0)):
+            ny, nx = y1 + dy, x1 + dx
+            if 0 <= ny < N and 0 <= nx < M and visited[ny][nx][wall] == 0:
+                if room[ny][nx] == 0: # 지역이면 가기
+                    visited[ny][nx][wall] = visited[y1][x1][wall] + 1
+                    Q.append((ny,nx,wall))
+
+                elif room[ny][nx] and not wall: # 벽인데 아직 안뚫었다면?
+                    visited[ny][nx][True] = visited[y1][x1][wall] + 1
+                    Q.append((ny,nx,True)) # flag들어주기
+
     return -1
 
-n, m = map(int,input().split())
-li = [list(map(int,input().rstrip())) for _ in range(n)]
-visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+# 입력값 받기
+N, M = map(int,input().split())
+room = [list(map(int,input())) for i in range(N)]
+visited = [[[0, 0] for j in range(M)] for k in range(N)]
+
+visited[0][0][False] = 1
 print(bfs())
