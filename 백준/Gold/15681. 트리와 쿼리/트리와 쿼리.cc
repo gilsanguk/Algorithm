903fdab19a50c71eb9418be57af8496fb5a36f45
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2023-03-15 17:34:27 +0900

    [Gold V] Title: 트리와 쿼리, Time: 80 ms, Memory: 12352 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#define MAX 100001
 
-const int MAX = 1000005;
+using namespace std;
 
 int N, R, Q;
 vector<int> graph[MAX];
 int tree[MAX];
+int u, v;
 
 int dfs(int cur, int parent) {
     tree[cur] = 1;
-    for (int next : graph[cur]) {
+    for (int next: graph[cur]) {
         if (next != parent) {
             tree[cur] += dfs(next, cur);
         }
@@ -24,14 +25,12 @@ int main() {
 #endif
     scanf("%d %d %d", &N, &R, &Q);
     for (int i = 0; i < N - 1; i++) {
-        int u, v;
         scanf("%d %d", &u, &v);
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
     dfs(R, 0);
     for (int i = 0; i < Q; i++) {
-        int u;
         scanf("%d", &u);
         printf("%d\n", tree[u]);
     }
