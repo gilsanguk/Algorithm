8b8a58bff97bff294052347d145f7589e70f6d33
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2023-04-03 20:59:53 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,10 @@
+def solution(name, yearning, photo):
+    answer = []
+    for i in photo:
+        result = 0
+        for j in i:
+            for k in range(len(name)):
+                if j == name[k]:
+                    result += yearning[k]
+        answer.append(result)
+    return answer
