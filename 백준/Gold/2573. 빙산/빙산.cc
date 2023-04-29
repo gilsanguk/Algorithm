2162aaba59d38dfda3c8ed80d068f6149c314222
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2023-04-29 23:11:09 +0900

    [Gold IV] Title: 빙산, Time: 92 ms, Memory: 2820 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -8,84 +8,61 @@ int N, M;
 int dy[4] = {-1, 0, 1, 0};
 int dx[4] = {0, 1, 0, -1};
 int arr[301][301];
-int visited[301][301];
+bool visited[301][301];
+int counted[301][301];
 struct Node {
     int y, x;
 };
+queue<Node> q;
 
-int check() {
-    memset(visited, 0, sizeof(visited));
-    queue<Node> q;
-    int ret = 0;
-    for (int y = 0; y < N; y++) {
-        for (int x = 0; x < M; x++) {
-            if (arr[y][x] == 0 || visited[y][x] == 1) continue;
-            if (ret == 1) return 2;
-            ret = 1;
-            q.push({y, x});
-            visited[y][x] = 1;
-            while (!q.empty()) {
-                Node cur = q.front();
-                q.pop();
-                for (int i = 0; i < 4; i++) {
-                    int ny = cur.y + dy[i], nx = cur.x + dx[i];
-                    if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-                    if (visited[ny][nx] || arr[ny][nx] == 0) continue;
-                    visited[ny][nx] = 1;
-                    q.push({ny, nx});
-                }
-            }
-        }
-    }
-    return ret;
-}
-
-void bfs(int y, int x) {
-    queue<Node> q;
-    q.push({y, x});
-    visited[y][x] = -1;
+void bfs() {
     while (!q.empty()) {
         Node cur = q.front();
         q.pop();
         for (int i = 0; i < 4; i++) {
             int ny = cur.y + dy[i], nx = cur.x + dx[i];
             if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-            if (visited[ny][nx] == -1) continue;
             if (arr[ny][nx] == 0) {
-                visited[ny][nx] = -1;
-                q.push({ny, nx});
+                counted[cur.y][cur.x]++;
             }
             else {
-                visited[ny][nx]++;
+                if (visited[ny][nx]) continue;
+                visited[ny][nx] = true;
+                q.push({ny,nx});
             }
         }
     }
 }
 
 int solve() {
-    int ret = 0;
-    int flag;
+    int cnt = 0;
     while (true) {
-        flag = check();
-        if (flag != 1) break;
-        ret++;
-        memset(visited, 0, sizeof(visited));
+        int ret = 0;
         for (int y = 0; y < N; y++) {
             for (int x = 0; x < M; x++) {
-                if (arr[y][x] == 0 && visited[y][x] == 0) {
-                    bfs(y, x);
-                }
+                if (arr[y][x] == 0 || visited[y][x]) continue;
+                if (ret == 1) return cnt;
+                ret = 1;
+                q.push({y, x});
+                visited[y][x] = true;
+                bfs();
+            }
+        }
+        if (ret == 0) return 0;
+        for (int y = 0; y < N; y++) {
+            for (int x = 0; x < M; x++) {
+                if (!visited[y][x]) continue;
+                arr[y][x] = max(arr[y][x] - counted[y][x], 0);
             }
         }
         for (int y = 0; y < N; y++) {
             for (int x = 0; x < M; x++) {
-                if (visited[y][x] == -1) continue;
-                arr[y][x] = max(arr[y][x] - visited[y][x], 0);
+                visited[y][x] = false;
+                counted[y][x] = 0;
             }
         }
+        cnt++;
     }
-    if (flag == 0) ret = 0;
-    return ret;
 }
 
 int main() {
