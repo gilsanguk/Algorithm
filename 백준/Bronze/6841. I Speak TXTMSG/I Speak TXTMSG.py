f819fb615b75e93d68231d542a1f6105f292a796
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2023-06-24 19:04:44 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 36 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,6 @@
+a={'CU':'see you',':-)':'I’m happy',':-(':'I’m unhappy',';-)':'wink',':-P':'stick out my tongue','(~.~)':'sleepy','TA':'totally awesome','CCC':'Canadian Computing Competition','CUZ':'because','TY':'thank-you','YW':'you’re welcome','TTYL':'talk to you later'}
+while True:
+    try:
+        b=input()
+        print(a[b] if b in a else b)
+    except:break
