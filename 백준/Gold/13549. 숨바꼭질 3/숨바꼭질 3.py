9c4604d02c6187835a875df8f84e58025fdddc45
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2023-06-28 21:13:03 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 120 ms, Memory: 34276 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -9,12 +9,12 @@ def bfs(s):
         if 0 <= s * 2 <= 100000 and not visited[s*2]:
             visited[s*2] = True
             q.appendleft((s*2,d))
+        if 0 <= s - 1 <= 100000 and not visited[s - 1]:
+            visited[s - 1] = True
+            q.append((s - 1, d + 1))
         if 0 <= s + 1 <= 100000 and not visited[s+1]:
             visited[s+1] = True
             q.append((s+1,d+1))
-        if 0 <= s - 1 <= 100000 and not visited[s-1]:
-            visited[s-1] = True
-            q.append((s-1,d+1))
 
 
 n, k = map(int,input().split())
