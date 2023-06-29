dc82db0142335b1857a4d47701f505d694ede9ed
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2023-06-30 00:49:46 +0900

    [Gold V] Title: 경쟁적 전염, Time: 36 ms, Memory: 2792 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.cc
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.cc
@@ -1,33 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
 struct Point {
     int y, x;
 };
-vector<Point> elem[1001];
+queue<Point> elem[1001];
 int N, K;
 int arr[202][202];
 
 int dy[4] = {-1, 0, 1, 0};
 int dx[4] = {0, 1, 0, -1};
 void breed() {
-    vector<Point> tmp[1001];
     for (int i = 1; i <= K; i++) {
-        for (Point& p : elem[i]) {
+        int qSize = elem[i].size();
+        for (int q = 0; q < qSize; q++) {
+            Point p = elem[i].front();
+            elem[i].pop();
             for (int d = 0; d < 4; d++) {
                 int ny = p.y + dy[d], nx = p.x + dx[d];
                 if (ny < 1 || ny > N || nx < 1 || nx > N) continue;
                 if (arr[ny][nx] != 0) continue;
                 arr[ny][nx] = i;
-                tmp[i].push_back({ny, nx});
+                elem[i].push({ny, nx});
             }
-        }
-    }
-    for (int i = 1; i <= K; i++) {
-        for (Point& p : tmp[i]) {
-            elem[i] = vector<Point>(tmp[i].begin(), tmp[i].end());
         }
     }
 }
@@ -39,7 +36,7 @@ void solve() {
         for (int x = 1; x <= N; x++) {
             scanf("%d", &arr[y][x]);
             if (arr[y][x] != 0)
-                elem[arr[y][x]].push_back({y, x});
+                elem[arr[y][x]].push({y, x});
         }
     }
     scanf("%d %d %d", &S, &Y, &X);
