8e5788afb9d170959d5b313981c4a06b14697ee2
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2023-08-19 13:58:13 +0900

    [Gold V] Title: 암호코드, Time: 140 ms, Memory: 15340 KB -BaekjoonHub

---
+++ 백준/Gold/2011. 암호코드/암호코드.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N;
+    static String cipher;
+
+    static Long MOD(Long x) {return x % 1000000;}
+
+    static void input() throws IOException {
+        cipher = br.readLine();
+        N = cipher.length();
+        memo = new Long[N];
+        Arrays.fill(memo, -1L);
+    }
+
+    static Long[] memo;
+    static Long solve(int n) {
+        if (n == N) return 1L;
+        if (memo[n] != -1) return memo[n];
+        memo[n] = 0L;
+        if (n < N - 1) {
+            if (cipher.charAt(n) != '0')
+                memo[n] = MOD(memo[n] + solve(n + 1));
+            int tmp = Integer.parseInt(cipher.substring(n, n + 2));
+            if (9 < tmp && tmp < 27)
+                memo[n] = MOD(memo[n] + solve(n + 2));
+        } else {
+            if (cipher.charAt(n) != '0')
+                memo[n] = MOD(memo[n] + solve(n + 1));
+        }
+        return memo[n];
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        bw.write(solve(0) + "");
+        bw.flush();
+        bw.close();
+    }
+}
