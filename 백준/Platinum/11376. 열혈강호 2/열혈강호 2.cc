c65664c37f4872b5e080dad378ac4239ae41c3d4
Gil sang-uk <109332551+gilsanguk@users.noreply.github.com>
2023-08-29 11:42:20 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 964 ms, Memory: 6008 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int N, M;
+vector<int> graph[1001];
+
+void input() {
+    int cnt, n;
+    scanf("%d %d", &N, &M);
+    for (int i = 1; i <= N; i++) {
+        scanf("%d", &cnt);
+        for (int j = 0; j < cnt; j++) {
+            scanf("%d", &n);
+            graph[i].push_back(n);
+        }
+    }
+}
+
+int matched[1001];
+bool visited[1001];
+int dfs(int x) {
+    for (int nx: graph[x]) {
+        if (visited[nx]) continue;
+        visited[nx] = true;
+        if (!matched[nx] || dfs(matched[nx])) {
+            matched[nx] = x;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void solve() {
+    int ans = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j < 2; j++) {
+            memset(visited, false, sizeof(visited));
+            ans += dfs(i);
+        }
+    }
+    printf("%d", ans);
+}
+
+int main() {
+    input();
+    solve();
+}
